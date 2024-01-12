23c20fbbd0e08ed7e1fc8a6ec203522d0a835393
sahar-amjad1 <150724877+sahar-amjad1@users.noreply.github.com>
2024-01-12 18:02:03 +0500

    Add files via upload

---
+++ to-do.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+let todoString = [];
+let loop = true;
+while (loop) {
+    const TodoInput = await inquirer.prompt([
+        {
+            type: "input",
+            name: "todoItem",
+            message: "Enter item Name which you want to add in your To-Do list"
+        },
+        {
+            type: "confrim",
+            name: "addMore",
+            message: "Do you want to add more item in your To-Do list?",
+            default: false
+        }
+    ]);
+    const todoItem = TodoInput.todoitem;
+    const addMore = TodoInput.addMore;
+    if (todoItem) {
+        todoString.push(todoItem);
+    }
+    loop = addMore;
+}
+;
+if (todoString.length > 0) {
+    console.log(`Here is your To-Do list`);
+    {
+    }
+}
+else {
+    console.log(`Your To-Do list is Empty!`);
+}
